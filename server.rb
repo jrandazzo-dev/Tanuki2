2f94a9f55f8e711d00e94cef411d0bbfe479237b
Joshua Lambert <joshua@gitlab.com>
2017-08-13 18:13:29 +0000

    Update server.rb

--- server.rb
+++ server.rb
@@ -1,10 +1,18 @@
 require 'webrick'
+require 'securerandom'
 
 server = WEBrick::HTTPServer.new :Port => 5000
 
 #The following proc is used to customize the server operations
 server.mount_proc '/' do |request, response|
-  response.body = 'Hello, world!'
+  response.body = 'Hello, improved world!'
+  sleep SecureRandom.random_number
+end
+
+server.mount_proc '/error' do |request, response|
+  response.status = 500
+  response.body = 'Sorry we encountered an error.'
+  sleep 1.0+SecureRandom.random_number
 end
 
 server.start
