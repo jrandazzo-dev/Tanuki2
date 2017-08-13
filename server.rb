25a935ab158a95e408814016a60b6ea9d68d6e2c
Joshua Lambert <joshua@gitlab.com>
2017-08-13 18:22:04 +0000

    Update server.rb

--- server.rb
+++ server.rb
@@ -9,6 +9,7 @@ server.mount_proc '/' do |request, response|
   sleep 0.125 + SecureRandom.random_number*0.250
 end
 
+#Return an error to help demonstrate monitoring capabilities
 server.mount_proc '/error' do |request, response|
   response.status = 500
   response.body = 'Sorry we encountered an error.'
