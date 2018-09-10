9a22c4f5578c75803680f534afa85c9e75e94b33
David Astor <dastor@gitlab.com>
2018-09-10 16:46:28 +0000

    Update server.rb

--- server.rb
+++ server.rb
@@ -6,7 +6,7 @@ server = WEBrick::HTTPServer.new :Port => ENV.fetch('PORT'){ 5000 }
 #Return a successful Hello World response.
 server.mount_proc '/' do |_request, response|
   #TODO: use HTML
-  response.body = 'another test"
+  response.body = 'another test'
   sleep 0.125 + SecureRandom.random_number*0.250
 end
 
